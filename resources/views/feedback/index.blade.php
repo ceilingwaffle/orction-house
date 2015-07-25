64eec36fc95cdd86b4e243acd27b88f851502c9d
Ceiling Waffle <ceilingwaffle@gmail.com>
2015-07-26 00:00:38 +1000

    Show no results message when user has no feedback

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -10,37 +10,41 @@
                 </div>
                 <div class="panel-body">
                     @include('partials._validationErrors')
-                    - todo:  
-                    - Total positive
-                    - Total neutral
-                    - Total negative
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th>Type</th>
-                            <th>User</th>
-                            <th>Bought Item</th>
-                            <th>Message</th>
-                            <th>Written</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($feedbackData as $feedback)
+                    @if (count($feedbackData) < 1)
+                        <h3 class="text-center">No feedback to show</h3>
+                    @else
+                        - todo:
+                        - Total positive
+                        - Total neutral
+                        - Total negative
+                        <table class="table table-striped">
+                            <thead>
                             <tr>
-                                <td>{{ $feedback['feedback_type'] }}</td>
-                                <td>
-                                    <a href="/users/{{ $feedback['left_by_username'] }}/feedback">{{ $feedback['left_by_username'] }}</a>
-                                </td>
-                                <td>
-                                    <a href="/auctions/{{ $feedback['auction_id'] }}">{{ $feedback['auction_title'] }}</a>
-                                    (${{ $feedback['auction_winning_bid_amount'] }})
-                                </td>
-                                <td>{{ $feedback['feedback_message'] }}</td>
-                                <td>{{ $feedback['feedback_date'] }}</td>
+                                <th>Type</th>
+                                <th>User</th>
+                                <th>Bought Item</th>
+                                <th>Message</th>
+                                <th>Written</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @foreach ($feedbackData as $feedback)
+                                <tr>
+                                    <td>{{ $feedback['feedback_type'] }}</td>
+                                    <td>
+                                        <a href="/users/{{ $feedback['left_by_username'] }}/feedback">{{ $feedback['left_by_username'] }}</a>
+                                    </td>
+                                    <td>
+                                        <a href="/auctions/{{ $feedback['auction_id'] }}">{{ $feedback['auction_title'] }}</a>
+                                        (${{ $feedback['auction_winning_bid_amount'] }})
+                                    </td>
+                                    <td>{{ $feedback['feedback_message'] }}</td>
+                                    <td>{{ $feedback['feedback_date'] }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
