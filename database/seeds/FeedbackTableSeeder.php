70d2bcb64c51da25c4d74bbcae798f2dd037842d
Ceiling Waffle <ceilingwaffle@gmail.com>
2015-07-25 22:52:11 +1000

    Add user id to feedback table migration, and change the way feedback seeding works

--- database/seeds/FeedbackTableSeeder.php
+++ database/seeds/FeedbackTableSeeder.php
@@ -14,21 +14,39 @@ class FeedbackTableSeeder extends Seeder
     {
         $faker = Faker\Factory::create();
 
-        $auctions = App\Auction::all(['id'])->shuffle();
+        $auctions = App\Auction::all()->shuffle();
         $feedbackTypes = App\FeedbackType::all(['id']);
 
         foreach ($auctions as $auction) {
 
+            // No feedback if the auction has not ended yet
+            if (Carbon\Carbon::now()->lte(new Carbon\Carbon($auction->end_date))) {
+                continue;
+            }
+
+            $auctionWithBids = $auction->where('id', '=', $auction->id)
+                ->with([
+                    'bids' => function ($query) {
+                        $query->orderBy('amount', 'desc')->take(1);
+                    }
+                ])->first()->toArray();
+
+            // No feedback if no bids were placed
+            if (!isset($auctionWithBids['bids'][0])) {
+                continue;
+            }
+
+            $highestBid = $auctionWithBids['bids'][0];
+
             // Insert a random number of feedback records
-            if (rand(1,2) == 2) {
+            if (rand(1, 2) == 2) {
                 Feedback::create([
                     'auction_id' => $auction->id,
+                    'left_by_user_id' => $highestBid['user_id'],
                     'message' => $faker->sentence(12, $variableNbWords = true),
                     'feedback_type_id' => $feedbackTypes->random()->id,
                 ]);
             }
-
         }
-
     }
 }
