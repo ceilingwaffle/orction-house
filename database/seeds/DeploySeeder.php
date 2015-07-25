121e8d98cc2b68368958f2dc8675314cb12f321a
Ceiling Waffle <ceilingwaffle@gmail.com>
2015-07-25 16:34:52 +1000

    Add a seeder class to be used during the deployment process.

---
+++ database/seeds/DeploySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DeploySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        //$this->call(UserTableSeeder::class);
+        $this->call(AuctionCategoryTableSeeder::class);
+        $this->call(AuctionConditionTableSeeder::class);
+        //$this->call(AuctionTableSeeder::class);
+        $this->call(BidTableSeeder::class);
+        $this->call(FeedbackTypeTableSeeder::class);
+        //$this->call(FeedbackTableSeeder::class);
+        //$this->call(WinnerTableSeeder::class);
+
+        Model::reguard();
+    }
+}
